26be8c1191cf1d312b4816938abb8910b636eb6a
William Horvath <william@horvath.blog>
2026-08-08 14:55:08 +0900

    update diffcalc tool build to CMake

---
+++ tools/diffcalc/README.md
@@ -0,0 +1,12 @@
+### Building
+
+```
+cmake -S . -B build
+cmake --build build --parallel
+```
+
+### Usage
+
+```
+./build/diffcalc <map.osu> [speed] [0xMODFLAGS]
+```
---
+++ tools/diffcalc/pch.h
@@ -0,0 +1,41 @@
+#pragma once
+
+// glm, header only, template-heavy
+#include "glm/ext/vector_float1.hpp"
+#include "glm/ext/vector_float2.hpp"
+#include "glm/ext/vector_float3.hpp"
+#include "glm/ext/vector_float4.hpp"
+#include "glm/geometric.hpp"
+#include "glm/trigonometric.hpp"
+
+// commonly-included stdlib includes
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <cctype>
+#include <charconv>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <optional>
+#include <print>
+#include <span>
+#include <stop_token>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
+#include <utility>
+#include <variant>
+#include <vector>
