0bdc58d2349767b6f3b6c3a6377e68b3fabcb233
William Horvath <william@horvath.blog>
2026-09-23 16:07:14 +0900

    update changelog
    [no release]

--- src/App/Neomod/AboutScreen.cpp
+++ src/App/Neomod/AboutScreen.cpp
@@ -401,6 +401,7 @@ void AboutScreen::buildChangelog() {
     v43_14.title = "43.14 (" CHANGELOG_TIMESTAMP ")";
     v43_14.changes = {
         R"(- Added a preview panel to online beatmaps screen)",
+        R"(- Exposed "direct_autoselect" behavior as a checkbox)",
         R"(- Fixed startup crashing in some cases)",
         R"(- Allowed force disabling skins from setting convars)",
     };
